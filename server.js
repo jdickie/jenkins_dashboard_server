ad094ed0915f61d9ba5f8c783386e14a60a4d945
Grant Dickie <gdickie@npr.org>
2016-05-20 07:32:32 -0400

    Adding in cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var ServerRoutes = require('./lib/routes.js');
 var app = express();
+var cors = require('cors');
 const PORT = 3000;
 
+app.use(cors());
 app.use('/', ServerRoutes);
 
 app.listen(PORT, function() {
